a13fb8b57579e54187b25cd68c3647097a34f336
vagrant <vagrant@homestead>
2020-01-29 17:27:23 +0000

    image uploads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,10 +51,16 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <img src="/uploads/avatars/{{ $user->avatar }}" style="width:32px; height:32px; border-radius:50%; margin-right:5px;">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    
+                                    <a class="dropdown-item" href="{{ route('profile') }}">
+                                        Profile
+                                    </a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+
+                <div class="card-header">
+                <img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px">
+                    <h2>{{ $user->name }}'s Profile</h2>
+                </div>
+
+                <div class="card-body">
+                    <form enctype="multipart/form-data" action="/profile" method="POST">
+                        @csrf
+
+                        <h5>Update Profile Image</h5>
+                        <input type="file" name="avatar">
+                        <button type="submit" class="float-right btn-small btn btn-primary">Update</button>
+
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
